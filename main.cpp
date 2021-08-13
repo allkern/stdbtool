9b74fa8743e8242b68e110745ea65937b3434e9e
Lucas Eriksson <dracc@dracc.se>
2021-08-13 15:47:32 +0200

    Add primitive arg handling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include "stdb.hpp"
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::wstring name = L"test";
+    std::string path = "test";
+    if (argc >= 2) {
+        name = std::wstring(&argv[1][0], &argv[1][sizeof(argv[1])]);
+        path = argv[2];
+    }
     stdb::init();
-    stdb::create_soundtrack(L"test", "test");
+    stdb::create_soundtrack(name, path);
     stdb::write();
 }
